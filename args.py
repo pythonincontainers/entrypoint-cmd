c5a6e2fbd0483f2028f7cbf98e67649a4022dbb7
pythonincontainers <45329364+pythonincontainers@users.noreply.github.com>
2019-05-09 20:17:25 +0200

    Python code to display process' command line and list all processes

--- args.py
+++ args.py
@@ -1,8 +1,23 @@
-import sys
-import psutil
+import sys, signal, time, psutil
 
-print("This process command line: ", psutil.Process().cmdline())
-print("List of all processes:")
+signames = ['Zero!','SIGHUP','SIGINT','SIGQUIT','SIGILL','SIGTRAP','SIGABRT','SIGBUS','SIGFPE','SIGKILL','SIGUSR1','SIGSEGV','SIGUSR2','SIGPIPE','SIGALRM','SIGTERM','SIGSTKFLT','SIGCHLD','SIGCONT','SIGSTOP','SIGTSTP','SIGTTIN','SIGTTOU','SIGURG','SIGXCPU','SIGXFSZ','SIGVTALRM','SIGPROF','SIGWINCH','SIGIO','SIGPWR','SIGSYS']
 
-for proc in psutil.process_iter(attrs=['pid','ppid','cmdline']):
-    print("PID=",proc.info['pid']," PPID=",proc.info['ppid']," CMDLINE=",proc.info['cmdline'])
+def handler(signal, frame):
+    print('Signal received: ', signames[signal])
+    return
+
+if __name__ == '__main__':
+    print("This process PID: ", psutil.Process().pid)
+    print("This process command line: ", psutil.Process().cmdline())
+    print("List of all processes in current Container:")
+
+    for proc in psutil.process_iter(attrs=['pid','ppid','cmdline']):
+        print("PID=",proc.info['pid']," PPID=",proc.info['ppid']," CMDLINE=",proc.info['cmdline'])
+
+    if sys.argv[1] == 'signals':
+        signals_allowed = set(signal.Signals) - {signal.SIGKILL, signal.SIGSTOP}
+        for sig in signals_allowed:
+            signal.signal(sig, handler)
+        print("Waiting for signals, please send some :)")
+        while True:
+            signal.pause()
