073ae0d5b70211ae4a783fced081d4123d993369
pythonincontainers <45329364+pythonincontainers@users.noreply.github.com>
2019-05-14 17:56:34 +0200

    Python code to display process' command line and list all processes

--- args.py
+++ args.py
@@ -7,14 +7,16 @@ def handler(signal, frame):
     return
 
 if __name__ == '__main__':
-    print("This process PID: ", psutil.Process().pid)
-    print("This process command line: ", psutil.Process().cmdline())
-    print("List of all processes in current Container:")
+    print("This process PID is: ", psutil.Process().pid)
+    print("This process executable is: ", psutil.Process().exe())
+    print("This process sys.argv is: ", sys.argv)
+    print("This process command line is: ", psutil.Process().cmdline())
+    print("List of all processes in the current Container:")
 
     for proc in psutil.process_iter(attrs=['pid','ppid','cmdline']):
         print("PID=",proc.info['pid']," PPID=",proc.info['ppid']," CMDLINE=",proc.info['cmdline'])
 
-    if sys.argv[1] == 'signals':
+    if 'signals' in sys.argv:
         signals_allowed = set(signal.Signals) - {signal.SIGKILL, signal.SIGSTOP}
         for sig in signals_allowed:
             signal.signal(sig, handler)
